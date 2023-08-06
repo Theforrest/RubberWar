90eb7ade639b12e03c113ca90085a1297123d892
German <germannajardelgado#email.com>
2023-08-06 02:38:29 +0200

    Completed initial configurations and implemented an activity changing button

---
+++ app/src/main/java/com/example/rubberwar/SelectModeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rubberwar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SelectModeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_mode)
+    }
+}
